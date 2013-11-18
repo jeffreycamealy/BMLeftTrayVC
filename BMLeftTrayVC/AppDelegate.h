80329a42e1cfeb6cde9579734b7697553ff9e741
Jeffrey Camealy <jeffrey@orainteractive.com>
2013-11-18 11:14:05 -0600

    Initial Commit

---
+++ BMLeftTrayVC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BMLeftTrayVC
+//
+//  Created by Jeffrey Camealy on 11/18/13.
+//  Copyright (c) 2013 bearMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
